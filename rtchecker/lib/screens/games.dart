0e839a5da555bebaa3a02aa08c2aed96508caa3d
gsalazar2023 <gsalazar20@alumnos.utalca.cl>
2024-07-06 19:18:36 -0400

    reorg games

--- rtchecker/lib/screens/games.dart
+++ rtchecker/lib/screens/games.dart
@@ -38,19 +38,84 @@ class MyGameDts extends State<GameDts> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            tooltip: 'Search',
-            icon: const Icon(Icons.search),
-          ),
-        ],
-      ),
-      body: FutureBuilder<List<Game>>(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              tooltip: 'Search',
+              icon: const Icon(Icons.search),
+            ),
+          ],
+        ),
+        body: Container(
+            color: Color.fromARGB(255, 38, 43, 41),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FutureBuilder<List<Game>>(
+                  future: _gameFt,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error al cargar los juegos'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return Center(child: Text('No se encontraron juegos'));
+                    } else {
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 10.0,
+                          crossAxisSpacing: 10.0,
+                          childAspectRatio: 1.0,
+                        ),
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          Game game = snapshot.data![index];
+                          return GestureDetector(
+                            onTap: () {
+                              // Implementa la acción al hacer tap en un juego si es necesario
+                            },
+                            child: Card(
+                              color: Color.fromARGB(255, 39, 255, 201),
+                              elevation: 2.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  SvgPicture.asset(
+                                    game.icon,
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    game.name,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ],
+            )));
+  }
+}
+
+/*
+FutureBuilder<List<Game>>(
         future: _gameFt,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -101,7 +166,4 @@ class MyGameDts extends State<GameDts> {
             );
           }
         },
-      ),
-    );
-  }
-}
+      ), */
